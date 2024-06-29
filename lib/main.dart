7874b5b65aae4258b2f66864790f8fdc0a6621cc
MuhmedAshraf <100963016+MuhmedAshraf@users.noreply.github.com>
2024-06-30 01:58:58 +0300

    UI & cubit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:my_auth/screens/sign_in_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'cubit/user_cubit.dart';
+
+void main() {
+  runApp(
+    BlocProvider(
+      create: (context) => UserCubit(),
+      child: const Auth(),
+    ),
+  );
+}
+
+class Auth extends StatelessWidget {
+  const Auth({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SignInScreen(),
+    );
+  }
+}
